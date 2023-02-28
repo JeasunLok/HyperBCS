6bbfa553b06dd6a262c655efe98289a6082eb861
JeasunLok <luojsh7@mail2.sysu.edu.cn>
2023-03-01 00:28:03 +0800

    utils file : work

---
+++ utils/utils.py
@@ -0,0 +1,17 @@
+# a class for calculating the average of the accuracy and the loss
+#-------------------------------------------------------------------------------
+class AverageMeter(object):
+
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.average = 0 
+    self.sum = 0
+    self.count = 0
+
+  def update(self, val, n=1):
+    self.sum += val * n
+    self.count += n
+    self.average = self.sum / self.count
+#-------------------------------------------------------------------------------
