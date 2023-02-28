e5806d660f3832071b3f47103873b296739cbad5
JeasunLok <luojsh7@mail2.sysu.edu.cn>
2023-03-01 01:22:46 +0800

    add : other models file

---
+++ models/other_models.py
@@ -0,0 +1,40 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torch.optim as optim
+from torch.nn import init
+
+class MLP_4(nn.Module):
+    """
+    multi-layers perception with 4 layers as a baseline network
+    """
+    def weight_init(m):
+        if isinstance(m, nn.Linear):
+            init.kaiming_normal_(m.weight)
+            init.zeros_(m.bias)
+
+    def __init__(self, input_channels, num_classes, dropout=False):
+        super(MLP_4, self).__init__()
+        self.use_dropout = dropout
+        if dropout:
+            self.dropout = nn.Dropout(p=0.5)
+
+        self.fc1 = nn.Linear(input_channels, 2048)
+        self.fc2 = nn.Linear(2048, 4096)
+        self.fc3 = nn.Linear(4096, 2048)
+        self.fc4 = nn.Linear(2048, num_classes)
+
+        # self.apply(self.weight_init)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        if self.use_dropout:
+            x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        if self.use_dropout:
+            x = self.dropout(x)
+        x = F.relu(self.fc3(x))
+        if self.use_dropout:
+            x = self.dropout(x)
+        x = self.fc4(x)
+        return x
