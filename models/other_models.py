f0289c20afbe1814042a5a563f71d6cb51f02177
JeasunLok <luojsh7@mail2.sysu.edu.cn>
2023-03-04 01:47:02 +0800

    add : CNN and RNN models

--- models/other_models.py
+++ models/other_models.py
@@ -1,3 +1,4 @@
+import math
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
@@ -37,4 +38,298 @@ class MLP_4(nn.Module):
         if self.use_dropout:
             x = self.dropout(x)
         x = self.fc4(x)
+        return x
+    
+class CNN_1D(nn.Module):
+    """
+    CNN_!D model
+    """
+
+    def weight_init(m):
+        # [All the trainable parameters in our CNN should be initialized to
+        # be a random value between −0.05 and 0.05.]
+        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv1d):
+            init.uniform_(m.weight, -0.05, 0.05)
+            init.zeros_(m.bias)
+
+    def _get_final_flattened_size(self):
+        with torch.no_grad():
+            x = torch.zeros(1, 1, self.input_channels)
+            x = self.pool(self.conv(x))
+        return x.numel()
+
+    def __init__(self, input_channels, num_classes, kernel_size=None, pool_size=None):
+        super(CNN_1D, self).__init__()
+        if kernel_size is None:
+            # [In our experiments, k1 is better to be [ceil](n1/9)]
+            kernel_size = math.ceil(input_channels / 9)
+        if pool_size is None:
+            # The authors recommand that k2's value is chosen so that the pooled features have 30~40 values
+            # ceil(kernel_size/5) gives the same values as in the paper so let's assume it's okay
+            pool_size = math.ceil(kernel_size / 5)
+        self.input_channels = input_channels
+
+        # [The first hidden convolution layer C1 filters the n1 x 1 input data with 20 kernels of size k1 x 1]
+        self.conv = nn.Conv1d(1, 20, kernel_size)
+        self.pool = nn.MaxPool1d(pool_size)
+        self.features_size = self._get_final_flattened_size()
+        # [n4 is set to be 100]
+        self.fc1 = nn.Linear(self.features_size, 100)
+        self.fc2 = nn.Linear(100, num_classes)
+        # self.apply(self.weight_init)
+
+    def forward(self, x):
+        # [In our design architecture, we choose the hyperbolic tangent function tanh(u)]
+        x = x.squeeze(dim=-1).squeeze(dim=-1)
+        x = x.unsqueeze(1)
+        x = self.conv(x)
+        x = torch.tanh(self.pool(x))
+        x = x.view(-1, self.features_size)
+        x = torch.tanh(self.fc1(x))
+        x = self.fc2(x)
+        return x
+    
+class CNN_2D(nn.Module):
+    """
+    CNN_2D model
+    """
+
+    def weight_init(m):
+        if isinstance(m, (nn.Linear, nn.Conv3d)):
+            init.kaiming_normal_(m.weight)
+            init.zeros_(m.bias)
+
+    def __init__(self, input_channels, num_classes, patch_size=64):
+        super(CNN_2D, self).__init__()
+        self.input_channels = input_channels
+        self.patch_size = patch_size
+
+        # An input image of size 263x263 pixels is fed to conv1
+        # with 96 kernels of size 6x6x96 with a stride of 2 pixels
+        self.conv1 = nn.Conv3d(1, 96, (input_channels, 6, 6), stride=(1, 2, 2))
+        self.conv1_bn = nn.BatchNorm3d(96)
+        self.pool1 = nn.MaxPool3d((1, 2, 2))
+        #  256 kernels of size 3x3x256 with a stride of 2 pixels
+        self.conv2 = nn.Conv3d(1, 256, (96, 3, 3), stride=(1, 2, 2))
+        self.conv2_bn = nn.BatchNorm3d(256)
+        self.pool2 = nn.MaxPool3d((1, 2, 2))
+        # 512 kernels of size 3x3x512 with a stride of 1 pixel
+        self.conv3 = nn.Conv3d(1, 512, (256, 3, 3), stride=(1, 1, 1))
+        # Considering those large kernel values, I assume they actually merge the
+        # 3D tensors at each step
+
+        self.features_size = self._get_final_flattened_size()
+
+        # The fc1 has 1024 outputs, where dropout was applied after
+        # fc1 with a rate of 0.5
+        self.fc1 = nn.Linear(self.features_size, 1024)
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(1024, num_classes)
+
+        # self.apply(self.weight_init)
+
+    def _get_final_flattened_size(self):
+        with torch.no_grad():
+            x = torch.zeros(
+                (1, 1, self.input_channels, self.patch_size, self.patch_size)
+            )
+            x = F.relu(self.conv1_bn(self.conv1(x)))
+            x = self.pool1(x)
+            # print(x.size())
+            b, t, c, w, h = x.size()
+            x = x.view(b, 1, t * c, w, h)
+            x = F.relu(self.conv2_bn(self.conv2(x)))
+            x = self.pool2(x)
+            # print(x.size())
+            b, t, c, w, h = x.size()
+            x = x.view(b, 1, t * c, w, h)
+            x = F.relu(self.conv3(x))
+            # print(x.size())
+            _, t, c, w, h = x.size()
+        return t * c * w * h
+
+    def forward(self, x):
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        x = self.pool1(x)
+        b, t, c, w, h = x.size()
+        x = x.view(b, 1, t * c, w, h)
+        x = F.relu(self.conv2_bn(self.conv2(x)))
+        x = self.pool2(x)
+        b, t, c, w, h = x.size()
+        x = x.view(b, 1, t * c, w, h)
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, self.features_size)
+        x = self.fc1(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+    
+class CNN_3D(nn.Module):
+    """
+    CNN_3D model
+    """
+
+    @staticmethod
+    def weight_init(m):
+        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv3d):
+            init.xavier_uniform_(m.weight.data)
+            init.constant_(m.bias.data, 0)
+
+    def __init__(self, input_channels, num_classes, n_planes=2, patch_size=5):
+        super(CNN_3D, self).__init__()
+        self.input_channels = input_channels
+        self.n_planes = n_planes
+        self.patch_size = patch_size
+
+        # The proposed 3D-CNN model has two 3D convolution layers (C1 and C2)
+        # and a fully-connected layer (F1)
+        # we fix the spatial size of the 3D convolution kernels to 3 × 3
+        # while only slightly varying the spectral depth of the kernels
+        # for the Pavia University and Indian Pines scenes, those in C1 and C2
+        # were set to seven and three, respectively
+        self.conv1 = nn.Conv3d(1, n_planes, (7, 3, 3), padding=(1, 0, 0))
+        # the number of kernels in the second convolution layer is set to be
+        # twice as many as that in the first convolution layer
+        self.conv2 = nn.Conv3d(n_planes, 2 * n_planes, (3, 3, 3), padding=(1, 0, 0))
+        # self.dropout = nn.Dropout(p=0.5)
+        self.features_size = self._get_final_flattened_size()
+
+        self.fc = nn.Linear(self.features_size, num_classes)
+
+        # self.apply(self.weight_init)
+
+    def _get_final_flattened_size(self):
+        with torch.no_grad():
+            x = torch.zeros(
+                (1, 1, self.input_channels, self.patch_size, self.patch_size)
+            )
+            x = self.conv1(x)
+            x = self.conv2(x)
+            _, t, c, w, h = x.size()
+        return t * c * w * h
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, self.features_size)
+        # x = self.dropout(x)
+        x = self.fc(x)
+        return x
+    
+class CNN_3D_Classifer_1D(nn.Module):
+    """
+    CNN_3D_Classifer_1D model
+    """
+
+    def weight_init(m):
+        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv3d):
+            init.kaiming_normal_(m.weight)
+            init.zeros_(m.bias)
+
+    def __init__(self, input_channels, num_classes, patch_size=5, dilation=1):
+        super(CNN_3D_Classifer_1D, self).__init__()
+        # The first layer is a (3,3,3) kernel sized Conv characterized
+        # by a stride equal to 1 and number of neurons equal to 20
+        self.patch_size = patch_size
+        self.input_channels = input_channels
+        dilation = (dilation, 1, 1)
+
+        if patch_size == 3:
+            self.conv1 = nn.Conv3d(
+                1, 20, (3, 3, 3), stride=(1, 1, 1), dilation=dilation, padding=1
+            )
+        else:
+            self.conv1 = nn.Conv3d(
+                1, 20, (3, 3, 3), stride=(1, 1, 1), dilation=dilation, padding=0
+            )
+        # Next pooling is applied using a layer identical to the previous one
+        # with the difference of a 1D kernel size (1,1,3) and a larger stride
+        # equal to 2 in order to reduce the spectral dimension
+        self.pool1 = nn.Conv3d(
+            20, 20, (3, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0)
+        )
+        # Then, a duplicate of the first and second layers is created with
+        # 35 hidden neurons per layer.
+        self.conv2 = nn.Conv3d(
+            20, 35, (3, 3, 3), dilation=dilation, stride=(1, 1, 1), padding=(1, 0, 0)
+        )
+        self.pool2 = nn.Conv3d(
+            35, 35, (3, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0)
+        )
+        # Finally, the 1D spatial dimension is progressively reduced
+        # thanks to the use of two Conv layers, 35 neurons each,
+        # with respective kernel sizes of (1,1,3) and (1,1,2) and strides
+        # respectively equal to (1,1,1) and (1,1,2)
+        self.conv3 = nn.Conv3d(
+            35, 35, (3, 1, 1), dilation=dilation, stride=(1, 1, 1), padding=(1, 0, 0)
+        )
+        self.conv4 = nn.Conv3d(
+            35, 35, (2, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0)
+        )
+
+        # self.dropout = nn.Dropout(p=0.5)
+
+        self.features_size = self._get_final_flattened_size()
+        # The architecture ends with a fully connected layer where the number
+        # of neurons is equal to the number of input classes.
+        self.fc = nn.Linear(self.features_size, num_classes)
+
+        # self.apply(self.weight_init)
+
+    def _get_final_flattened_size(self):
+        with torch.no_grad():
+            x = torch.zeros(
+                (1, 1, self.input_channels, self.patch_size, self.patch_size)
+            )
+            x = self.pool1(self.conv1(x))
+            x = self.pool2(self.conv2(x))
+            x = self.conv3(x)
+            x = self.conv4(x)
+            _, t, c, w, h = x.size()
+        return t * c * w * h
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = x.view(-1, self.features_size)
+        # x = self.dropout(x)
+        x = self.fc(x)
+        return x
+    
+class RNN_1D(nn.Module):
+    """
+    RNN_1D
+    """
+
+    def weight_init(m):
+        # All weight matrices in our RNN and bias vectors are initialized with a uniform distribution, and the values of these weight matrices and bias vectors are initialized in the range [−0.1,0.1]
+        if isinstance(m, (nn.Linear, nn.GRU)):
+            init.uniform_(m.weight.data, -0.1, 0.1)
+            init.uniform_(m.bias.data, -0.1, 0.1)
+
+    def __init__(self, input_channels, num_classes):
+        # The proposed network model uses a single recurrent layer that adopts our modified GRUs of size 64 with sigmoid gate activation and PRetanh activation functions for hidden representations
+        super(RNN_1D, self).__init__()
+        self.input_channels = input_channels
+        self.gru = nn.GRU(1, 64, 1, bidirectional=False)  # TODO: try to change this ?
+        self.gru_bn = nn.BatchNorm1d(64 * input_channels)
+        self.tanh = nn.Tanh()
+        self.fc = nn.Linear(64 * input_channels, num_classes)
+
+    def forward(self, x):
+        x = x.squeeze()
+        x = x.unsqueeze(0)
+        # x is in 1, N, C but we expect C, N, 1 for GRU layer
+        x = x.permute(2, 1, 0)
+        x = self.gru(x)[0]
+        # x is in C, N, 64, we permute back
+        x = x.permute(1, 2, 0).contiguous()
+        x = x.view(x.size(0), -1)
+        x = self.gru_bn(x)
+        x = self.tanh(x)
+        x = self.fc(x)
         return x
